fcac306d783a9fb8e65eeb125168c3c56a80d2d6
diligentcrush <keshav.pothireddy@gmail.com>
2018-11-17 18:01:50 -0500

    Initial commit

---
+++ AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Splash
+//
+//  Created by Keshav Pothireddy on 11/17/18.
+//  Copyright © 2018 UWAVES. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+     
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+
+}
+
